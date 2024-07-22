41a36afb35beb7941d58bae28eb2d83ba7f7f9e9
Arghya <arghyadas242004@gmail.com>
2024-07-23 00:28:32 +0530

    fix ui

--- client/src/components/reusables/DropDown.tsx
+++ client/src/components/reusables/DropDown.tsx
@@ -58,13 +58,13 @@ export default function ComboboxDemo({ title, datas, setState, State }: Props) {
           variant="outline"
           role="combobox"
           aria-expanded={open}
-          className="justify-between w-[140px] md:w-[150px] text-black"
+          className="justify-between w-[140px] md:w-[180px] text-black"
         >
           {value ? datas.find((data) => data.name === value)?.name : title}
           <ChevronsUpDown className="opacity-50 ml-2 w-4 h-4 shrink-0" />
         </Button>
       </PopoverTrigger>
-      <PopoverContent className="p-0 w-[140px] md:w-[150px] max-h-60 text-black text-sm md:text-base overflow-y-auto">
+      <PopoverContent className="p-0 w-[140px] md:w-[180px] max-h-60 text-black text-sm md:text-base overflow-y-auto">
         <Command>
           <CommandInput placeholder={title} />
           <CommandList className="text-black">
